4588462886999d77ce0ae3c7111229b906d9d416
Onkar Nene <onkar.nene@effective.digital>
2019-08-17 21:04:04 +0530

    Implement splash screen with navigation.

--- app/src/main/java/com/onkar/android/gojekdemo/views/SplashActivity.kt
+++ app/src/main/java/com/onkar/android/gojekdemo/views/SplashActivity.kt
@@ -6,14 +6,33 @@
 
 package com.onkar.android.gojekdemo.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.onkar.android.gojekdemo.R.layout
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), Runnable {
+	
+	private val handler: Handler = Handler()
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(layout.activity_splash)
+	}
+	
+	override fun onStart() {
+		super.onStart()
+		handler.postDelayed(this, 2000)
+	}
+	
+	override fun onStop() {
+		super.onStop()
+		handler.removeCallbacks(this)
+	}
+	
+	override fun run() {
+		startActivity(Intent(this, TrendingActivity::class.java))
+		finish()
 	}
 }
